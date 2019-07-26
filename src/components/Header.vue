757632eafd99577fe82fc1413947e8a459d16c8c
anmoya <anmoya@uc.cl>
2019-07-25 22:38:46 -0400

    first

---
+++ src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <b-nav tabs>
+      
+      <b-nav-item disabled><b>Quiz</b></b-nav-item>
+      <b-nav-item disabled>Counter: {{ currentIndex+1 }}/{{ totalQuestions }}</b-nav-item>
+    </b-nav>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    totalQuestions: Number,
+    currentIndex: Number
+  }
+
+}
+</script>
+
---
+++ src/components/QuestionBox.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="question-box-container">
+    <div>
+      <b-jumbotron>
+        <template slot="lead">{{ currentQuestion.question }}</template>
+
+        
+        <hr class="my-4" />
+
+        <p v-for="(answer, index) in answers" :key="index">
+            {{ answer }}
+        </p>
+
+        <b-button variant="primary" href="#">Submit</b-button>
+        <b-button v-if="!(totalQuestions === 9)" @click="nextQuestion" variant="success" href="#">Next</b-button>
+        
+      </b-jumbotron>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    props: {
+        currentQuestion: Object,
+        nextQuestion: Function,
+        totalQuestions: Number
+    },
+    computed: {
+        answers() {
+            let answers = [...this.currentQuestion.incorrect_answers, this.currentQuestion.correct_answer];
+            return answers;
+        } 
+
+    }
+}
+</script>
+
